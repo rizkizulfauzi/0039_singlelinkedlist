3053e7abef043ca8325770a39da8ae7add9839f1
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-08 20:25:44 +0700

    COMMIT6

--- singlelingkedist.cpp
+++ singlelingkedist.cpp
@@ -4,57 +4,72 @@ using namespace std;
 
 class Node
 {
-    public:
-        int noMhs;
-        Node *next;
+public:
+    int noMhs;
+    Node *next;
 };
 
 class LinkedList
 {
     Node *START;
-public :
-LinkedList()
-{
-    START = NULL;
-}
 
-void addNode()
-{
-    int nim;
-    cout <<"\nMasukan Nomor Mahasiswa:";
-    cin >> nim;
-
-    Node *nodeBaru = new Node;
-    nodeBaru->noMhs =nim;
-
-    if (START != NULL || nim <= START->noMhs) 
+public:
+    LinkedList()
     {
-        if ((START != NULL) && (nim == START->noMhs))
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa:";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START != NULL || nim <= START->noMhs)
         {
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
             return;
         }
-        nodeBaru->next = START;
-        START =nodeBaru;
-        return;
-    }
-    Node *previous = START;
-    Node *current = START;
+        Node *previous = START;
+        Node *current = START;
 
-
-    while ((current != NULL) && (nim >= current->noMhs))
-    {
-        if (nim == current->noMhs)
+        while ((current != NULL) && (nim >= current->noMhs))
         {
-            cout << "\nDuplikasi noMhs tidak diijinkan\n";
-            return;
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
         }
-        previous =current;
-        current = current->next;
-
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
-    nodeBaru->next = current;
-    previous->next =nodeBaru;
-}
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+    bool search(int nim, Node **previous, Node **current)
+    {
+        *previous = *current;
+        *current - (*current)->next;
 
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous =*current;
+            *current =(*current)->next;
+        }
+        return (*current != NULL);
+    }
+  
 };
