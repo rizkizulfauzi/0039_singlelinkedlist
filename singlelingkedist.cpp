b84979ab9c58cd0dfe2e1b5fc1c0843fa42a1c26
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-08 20:19:44 +0700

    commit5

--- singlelingkedist.cpp
+++ singlelingkedist.cpp
@@ -31,8 +31,30 @@ void addNode()
     {
         if ((START != NULL) && (nim == START->noMhs))
         {
-            
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
         }
+        nodeBaru->next = START;
+        START =nodeBaru;
+        return;
     }
+    Node *previous = START;
+    Node *current = START;
+
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
+        previous =current;
+        current = current->next;
+
+    }
+    nodeBaru->next = current;
+    previous->next =nodeBaru;
 }
+
 };
