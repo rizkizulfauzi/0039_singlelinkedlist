881e0da1b7d6812b42c3b7f27b05798824a79b14
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-08 20:47:52 +0700

    commit10;

--- singlelingkedist.cpp
+++ singlelingkedist.cpp
@@ -123,5 +123,31 @@ int main()
         cout << endl
              <<"Masukan pilihan (1-5):";
         cin >> ch;
+        switch (ch)
+        {
+            case '1':
+            {
+                mhs.addNode();
+                break;
+            }
+            case '2':
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << endl
+                         << "List Kosong" <, endl;
+                         break;
+                } 
+                cout << endl
+                     << "\nMasukan no mahasiswa yang akan di hapus : ";
+                cin >> nim;
+                if (mhs.delNode(nim)== false)
+                cout << endl
+                     <<"Data tidak ditemukan" << endl;
+                else
+                     cout << endl
+                          <<"Data dengan nomor mahasiswa" << nim << "berhasil dihapus" <<endl;
+            }
+        }
     }
 }
