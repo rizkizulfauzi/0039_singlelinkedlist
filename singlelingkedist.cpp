e21cfaedec9c2ea45ca52170933c10d564b765c3
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-08 20:12:01 +0700

    commit4

--- singlelingkedist.cpp
+++ singlelingkedist.cpp
@@ -17,4 +17,22 @@ LinkedList()
 {
     START = NULL;
 }
+
+void addNode()
+{
+    int nim;
+    cout <<"\nMasukan Nomor Mahasiswa:";
+    cin >> nim;
+
+    Node *nodeBaru = new Node;
+    nodeBaru->noMhs =nim;
+
+    if (START != NULL || nim <= START->noMhs) 
+    {
+        if ((START != NULL) && (nim == START->noMhs))
+        {
+            
+        }
+    }
+}
 };
