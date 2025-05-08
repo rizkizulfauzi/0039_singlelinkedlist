727a776310d02be740a8174f8595f2d8ca9082be
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-05-08 20:33:05 +0700

    commit8

--- singlelingkedist.cpp
+++ singlelingkedist.cpp
@@ -66,22 +66,41 @@ public:
 
         while ((*current != NULL) && (nim != (*current)->noMhs))
         {
-            *previous =*current;
-            *current =(*current)->next;
+            *previous = *current;
+            *current = (*current)->next;
         }
         return (*current != NULL);
     }
     bool delNode(int nim)
     {
         Node *current, *previous;
-        if(!search(nim,&previous,&current))
-        return false;
+        if (!search(nim, &previous, &current))
+            return false;
         if (current == START)
-        START = START->next;
+            START = START->next;
         else
-        previous->next = current->next;
+            previous->next = current->next;
 
         delete current;
         return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode ->noMhs<< endl;
+                currentNode =currentNode->next; 
+            }
+            cout << endl;
+        }
     }
 };
